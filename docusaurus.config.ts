8b02b028c20cb5f84538b6002551f566b515f888
Matthew Hand <matthewhandau@gmail.com>
2024-10-29 10:14:42 +0000

    Status

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/open-webui/docs/blob/main",
           exclude: [
-            '**/getting-started/tab-**',
+            '**/getting-started/**/tab-**',
           ],
         },
         // blog: false,
